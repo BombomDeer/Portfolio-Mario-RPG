026fafaf568ae5c16ac8c7f28d1feb5a330d1328
BombomDeer <seanwon77@gmail.com>
2019-06-29 13:20:37 +0900

    Started work on battle scene and figured out lay out of characters

---
+++ Assets/02 Scripts/BattleScripts/CharacterB.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterB : MonoBehaviour
+{
+    int HP;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        InitStart();
+    }
+
+    private void OnEnable()
+    {
+        onEnable();
+    }
+    private void OnDisable()
+    {
+        onDisable();
+    }
+
+    virtual public void InitAwake()
+    {
+
+    }
+    virtual public void InitStart()
+    {
+
+    }
+
+    virtual public void onEnable()
+    {
+        Debug.Log("onEnable");
+    }
+
+    virtual public void onDisable()
+    {
+        Debug.Log("onDisable");
+    }
+
+    virtual public void UpdateDo()
+    {
+
+    }
+
+    private void Update()
+    {
+        UpdateDo();
+    }
+}
+
+
